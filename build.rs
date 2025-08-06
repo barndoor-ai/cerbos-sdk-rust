8c9282e1837429759cf6534f43c6e5be4259bb07
Peter Nehrer <pnehrer@eclipticalsoftware.com>
2025-08-06 14:42:20 -0600

    Upgrade to tonic and prost 0.14

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), std::io::Error> {
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .out_dir("src/genpb")
         .build_server(false)
+        .codec_path("tonic_prost::ProstCodec")        
         .compile_protos(&["proto/defs/cerbos/svc/v1/svc.proto"], &["proto/defs/"])
 }
